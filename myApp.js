3f204f7f009a2111dfe716fc74ce0e0de73c3f37
Chris Hyer <hyer.chris@gmail.com>
2023-08-17 16:01:49 -0700

    send html file on default route instead of string

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  const absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath);
 });
 
 
