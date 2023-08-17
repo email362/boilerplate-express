76029d40c751e6e5362e2a2da4a46a8918f6a745
Chris Hyer <hyer.chris@gmail.com>
2023-08-17 16:43:34 -0700

    added middleware to grab static files from public directory

--- myApp.js
+++ myApp.js
@@ -2,6 +2,10 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+const publicPath = __dirname + '/public';
+
+app.use('/public',express.static(publicPath));
+
 app.get("/", (req, res) => {
   const absolutePath = __dirname + '/views/index.html';
   res.sendFile(absolutePath);
