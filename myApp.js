c27af460dbe6a472e884899dd94a67882df559ae
Chris Hyer <hyer.chris@gmail.com>
2023-08-20 10:23:06 -0700

    added env var to change case of message

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
 let app = express();
 console.log("Hello World");
@@ -7,7 +8,11 @@ const publicPath = __dirname + '/public';
 app.use('/public',express.static(publicPath));
 
 app.get("/json", (req, res) => {
-  const returnObj = { "message": "Hello json" };
+  const isUpper = process.env.MESSAGE_STYLE === "uppercase";
+  let returnObj = { "message": "Hello json" };
+  if (isUpper) {
+    returnObj.message = returnObj.message.toUpperCase();
+  }
   res.json(returnObj);
 });
 
