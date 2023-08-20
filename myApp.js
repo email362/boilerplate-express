0c364cb3052f41284d43aba7abae75883c0991d1
Chris Hyer <hyer.chris@gmail.com>
2023-08-20 12:20:23 -0700

    res.send instead of res.json for /now route

--- myApp.js
+++ myApp.js
@@ -19,7 +19,7 @@ app.get('/now', (req, res, next) => {
   req.time = new Date().toString();
   next();
 }, (req, res) => {
-  res.json({time: req.time});
+  res.send({time: req.time});
 });
 
 app.get("/json", (req, res) => {
